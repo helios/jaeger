32a7104147486112ecb59edaed80a014b3e30e35
Oren Levy <oren@gethelios.dev>
2022-08-02 11:21:48 +0300

    add jaeger query build scripts

---
+++ scripts/build-upload-docker-jaeger-query.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euxf -o pipefail
+
+make create-baseimg-debugimg
+make build-binaries-linux
+
+docker buildx build \
+    --target release \
+    --build-arg base_image=localhost:5000/baseimg_alpine:latest \
+    --build-arg debug_image=localhost:5000/debugimg_alpine:latest \
+    --platform=linux/amd64 \
+    --file cmd/query/Dockerfile \
+    --tag $ECR_REGISTRY/$PROJECT_NAME:$COMMIT_HASH \
+    cmd/query
+
+docker tag $ECR_REGISTRY/$PROJECT_NAME:$COMMIT_HASH $ECR_REGISTRY/$PROJECT_NAME:latest
+
+# docker push $ECR_REGISTRY/$PROJECT_NAME:$COMMIT_HASH
+# docker push $ECR_REGISTRY/$PROJECT_NAME:latest
