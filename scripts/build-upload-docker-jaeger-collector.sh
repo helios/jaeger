465f5666a9823eaae2edfc0045eda545cf5c1603
David Gonoradsky <davidgonor123@gmail.com>
2023-07-30 15:47:36 +0300

    override collector build

---
+++ scripts/build-upload-docker-jaeger-collector.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euxf -o pipefail
+
+make create-baseimg-debugimg
+make build-binaries-linux
+
+docker buildx build \
+    --target release \
+    --build-arg base_image=localhost:5000/baseimg_alpine:latest \
+    --build-arg debug_image=localhost:5000/debugimg_alpine:latest \
+    --platform=linux/amd64 \
+    --file cmd/collector/Dockerfile \
+    --tag $ECR_REGISTRY/$PROJECT_NAME:$COMMIT_HASH \
+    --tag $ECR_REGISTRY/$PROJECT_NAME:latest \
+    --push \
+    cmd/collector
